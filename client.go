d9cf635fca65bc30ba495e5a31f7ecee0e434587
davide vanoncini <davidevanoncini2003@gmail.com>
2022-03-31 00:49:20 +0200

    client is only used for the struct, the method is not really used but it's useful to explain websockets

--- client.go
+++ client.go
@@ -1,22 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/gorilla/websocket"
 )
 
 type Client struct {
-	ID   string          //unique id that identifies a connection
-	Conn *websocket.Conn //websocket connection
-	Pool *Pool           //pool of connections (a client will be in a pool which on our case will represent an art show)
+	IDShowAttending int          //the id of the art show the user is attending
+	ID            string          //unique id that identifies a connection
+	Conn          *websocket.Conn //websocket connection
+	Pool          *Pool           //pool of connections (a client will be in a pool which on our case will represent an art show)
 }
 
-//Read method for client, it will read the like signal, save it into
-//the database and broadcast to the pool of clients the changes
+//Read method for client, it will read anything the client send to the server
+//for now we are just gonna print it and ignore it since we dont need the client to send anything
 //(the pointer before the Client means that this method will alter the values of the struct)
 func (c *Client) Read() {
 	//defer will run this function right before the function returns
+	//since it's a infinite loop it will run only if there is an error reading the message
+	//that's why we are disconnecting it and closing the connection
 	defer func() {
 		//it will send through the Disconnect channel the client that is being removed
 		//and close the connection
@@ -26,14 +30,15 @@ func (c *Client) Read() {
 
 	for {
 		//read the message from the client
-		messageType, p, err := c.Conn.ReadMessage()
+		_, p, err := c.Conn.ReadMessage()
 		//check for errors
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		//convert the message to the Message struct and breadcast it to the pool
-		like := Like{Type: messageType, Body: string(p)}
-		c.Pool.LikesHandler <- like
+		//print the message, we dont really need the user to send anything
+		//so we can just print it and ignore it for now
+		fmt.Printf("the client %s sent a message: %s\n", c.ID, string(p))
+		// c.Pool.LikesHandler <- like
 	}
 }
