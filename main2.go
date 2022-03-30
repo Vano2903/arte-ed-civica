eab3faf17581b089d39df992dae964fe34479859
davide vanoncini <davidevanoncini2003@gmail.com>
2022-03-31 00:47:26 +0200

    liked all endpoints

--- main2.go
+++ main2.go
@@ -1,57 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-type ArtShow struct {
-	Type      string `json:"type"`
-	Connected int    `json:"connected"`
-}
-
-type Like struct {
-	Type     string `json:"type"`
-	Likes    string `json:"likes"`
-	ID       string `json:"id"`
-	HasLikes bool   `json:"hasLikes"`
-}
-
-type LikeSent struct {
-	ArtPieceID string `json:"artPieceID"`
-	UserID     string `json:"userID"`
-	ArtShowID  string `json:"artShowID"`
-	AddedLike  bool   `json:"addedLike"`//if true it means that the user added a like, otherwise 
-}
-
-func serveWs(pool *Pool, w http.ResponseWriter, r *http.Request) {
-	fmt.Println("WebSocket Endpoint Hit")
-	//upgrade the connection of the
-	conn, err := Upgrade(w, r)
-	if err != nil {
-		fmt.Fprintf(w, "%+v\n", err)
-	}
-
-	client := &Client{
-		Conn: conn,
-		Pool: pool,
-	}
-
-	pool.Connect <- client
-	client.Read()
-}
-
-func setupRoutes() {
-	pool := NewPool()
-	go pool.Start()
-
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		serveWs(pool, w, r)
-	})
-}
-
 func main() {
-	fmt.Println("Distributed Chat App v0.01")
-	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	go handler.pool.Start()
+
+	r := mux.NewRouter()
+	r.Use(handler.loggerMiddleware)
+	admins := r.PathPrefix("/admins").Subrouter()
+	admins.HandleFunc("/artShows", handler.GetArtShowsForAdmins).Methods("GET")
+	admins.HandleFunc("/toggleShow/{id}", handler.ToggleShow).Methods("GET")
+
+	users := r.PathPrefix("/users").Subrouter()
+	users.HandleFunc("/artShows", handler.GetArtShowsForNormalUsers).Methods("GET")
+	users.HandleFunc("/getShow/{id}", handler.GetArtShow).Methods("GET")
+	users.HandleFunc("/toggleLike/{id}", handler.ToggleLike).Methods("GET")
+
+	socket := r.PathPrefix("/socket").Subrouter()
+	socket.HandleFunc("/connect/{id}", handler.ConnectUserToRealTimeUpdates).Methods("GET")
+
+	http.ListenAndServe(":8080", r)
 }
