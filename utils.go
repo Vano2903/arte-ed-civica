64e0a42198e1b36d48a1252264e951b31e520b07
davide vanoncini <davidevanoncini2003@gmail.com>
2022-04-01 08:33:22 +0000

    aaa

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
@@ -15,8 +16,18 @@ func connectToDB() (db *sql.DB, err error) {
 	return sql.Open("mysql", "root:root@tcp(localhost:3306)/arte?parseTime=true&charset=utf8mb4")
 }
 
-func removeFromSlice[T comparable](slice []T, i T) []T {
-	var toReturn []T
+// func removeFromSlice[T comparable](slice []T, i T) []T {
+// 	var toReturn []T
+// 	for _, v := range slice {
+// 		if v != i {
+// 			toReturn = append(toReturn, v)
+// 		}
+// 	}
+// 	return toReturn
+// }
+
+func removeFromSlice(slice []int, i int) []int {
+	var toReturn []int
 	for _, v := range slice {
 		if v != i {
 			toReturn = append(toReturn, v)
@@ -29,7 +40,7 @@ func init() {
 	//load the enviroment variables
 	err := godotenv.Load(".env")
 	if err != nil {
-		panic("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	handler = NewHandler()
